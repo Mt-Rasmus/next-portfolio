5f6811a3d4bdd76e6dc9557babe128fa791a945b
Mt-Rasmus <rasmus.stahl.47@gmail.com>
2026-02-10 15:07:58 +0100

    feat: refactor AdminPage to use AdminSection component and remove unnecessary elements

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,7 @@
 import { getProjects } from "@/app/services/api";
-import Link from "next/link";
-import AdminProjects from "./AdminProjects";
+import AdminSection from "@/app/components/AdminSection";
 
 export default async function AdminPage() {
   const projects = await getProjects();
-  return (
-    <div>
-      <Link href="/admin/projects/new">Create New Project</Link>
-      <AdminProjects projects={projects} />
-    </div>
-  );
+  return <AdminSection projects={projects} />;
 }
---
+++ src/app/components/AdminSection.tsx
@@ -0,0 +1,25 @@
+/** @jsxImportSource @emotion/react */
+"use client";
+import StyledButton from "@/app/components/StyledButton";
+import Link from "next/link";
+import { css } from "@emotion/react";
+import AdminProjects from "../admin/AdminProjects";
+import { Project } from "../types/project";
+
+const containerStyle = css`
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default function AdminPageClient({ projects }: { projects: Project[] }) {
+  return (
+    <div css={containerStyle}>
+      <Link href="/admin/projects/new" passHref>
+        <StyledButton>Create New Project</StyledButton>
+      </Link>
+      <AdminProjects projects={projects} />
+    </div>
+  );
+}
