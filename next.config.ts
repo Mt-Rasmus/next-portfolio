0bb13eef9ab1c6682d16d031cec816d1c54a22a2
Mt-Rasmus <rasmus.stahl.47@gmail.com>
2026-02-13 09:43:45 +0100

    feat: update image configuration to use remote patterns and integrate with plaiceholder

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,20 @@
 import type { NextConfig } from "next";
+import withPlaceholder from "@plaiceholder/next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
   },
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
+  turbopack: {},
 };
 
-export default nextConfig;
+export default withPlaceholder(nextConfig);
